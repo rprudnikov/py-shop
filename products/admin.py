16eaf7ebd8318b5726c226aa04e37156006bc075
rprudnikov <romanprudon@gmail.com>
2023-09-20 12:14:38 +0300

    configured offers for products

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -7,3 +11,4 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
