42858dc7054e93f2563b2979148b2c1dbddc8d70
rprudnikov <romanprudon@gmail.com>
2023-09-20 10:35:46 +0300

    added products page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request)
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('Hello New')
